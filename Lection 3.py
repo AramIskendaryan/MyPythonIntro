81c1b49629aeaf1150376f14559fc617a13b43d7
Aram Iskendaryan <aram.iskendaryan@acba.am>
2021-08-12 17:36:40 +0400

    First commit

---
+++ Lection 3.py
@@ -0,0 +1,15 @@
+ttt = 100
+
+if ttt == 10:
+    print("Switch is on")
+else:
+    print("Not ")
+
+
+# inp = int(input('Enter a number: '))
+#
+# if inp >= 10:
+#     print('The number is greater than 10')
+# if inp < 10:
+#     print('The number is less than 10')
+
---
+++ main.py
@@ -0,0 +1,44 @@
+l_1 = [5, 10, 15, 20, 25, 50, 20]
+
+print(l_1)
+
+l_2 = [5, 20, 15, 20, 25, 50, 20]
+
+l_2.pop(1)
+print(l_2)
+
+l_1.insert(3, 200)
+l_1.pop(4)
+print(l_1)
+
+x1 = [10, 20, 4]
+x2 = [70, 11, 20, 4, 100]
+
+x1.sort(reverse=True)
+print(x1[1])
+
+x2.sort(reverse=True)
+print(x2[1])
+
+dic1={1:10, 2:20}
+dic2={3:30, 4:40}
+dic3={5:50,6:60}
+
+dic1.update(dic2)
+dic1.update(dic3)
+print(dic1)
+
+P5 = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+
+# P5.pop('a')
+# print(P5)
+
+# P5.pop('e')
+# print(P5)
+
+s = {6, 4, 10, 20, 21, 3}
+
+print(s)
+
+print(90 in s)
+
