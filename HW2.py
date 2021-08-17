44f7e0e59cad5845b74af45c47a503ce813c8a34
AramIskendaryan <88795497+AramIskendaryan@users.noreply.github.com>
2021-08-17 17:59:37 +0400

    Add files via upload

---
+++ HW2.py
@@ -0,0 +1,69 @@
+# P1
+
+l = [5, 10, 15, 20, 25, 50, 20]
+x = l.index(20)
+
+if 20 in l:
+    l.pop(x)
+    l.insert(x, 200)
+print(l)
+
+# P2
+
+l_1 = [5, 20, 15, 20, 25, 50, 20]
+y = l_1.index(20)
+
+if 20 in l_1:
+    l_1.pop(y)
+    print(l_1)
+
+# P3
+"""
+n = int(input())
+l_2 = []
+for i in range(n):
+    l_2.append(int(input()))
+s = set(l_2)
+s1 = list(l_2)
+s1.sort(reverse=True)
+print(s1[1])
+"""
+# P4
+
+dic1 = {1: 10, 2: 20}
+dic2 = {3: 30, 4: 40}
+dic3 = {5: 50, 6: 60}
+dic4 = {}
+dic4.update(dic1)
+dic4.update(dic3)
+dic4.update(dic2)
+print(dic4)
+
+# P5
+
+dic_remove1 = {'a':1,'b':2,'c':3,'d':4}
+dic_remove1.pop('a')
+print(dic_remove1)
+
+dic_remove2 = {'a':1,'b':2,'c':3,'d':4}
+
+if 'e' in dic_remove2:
+    dic_remove2.pop('e')
+else:
+    print(dic_remove2)
+
+# P6
+
+s = {1, 10, 90, 9}
+n = 90
+if n in s:
+    print(True)
+
+# P7
+
+n = int(input())
+i = 0
+while i + 7 <= n:
+    i = i + 7
+    print(i)
+
