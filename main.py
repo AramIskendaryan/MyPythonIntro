6302d3fa98808e97078907545a3cf06f0d86adfe
aram.iskendaryan <aram.iskendaryan@gmail.com>
2021-08-27 12:50:58 +0400

    Homework 2

--- main.py
+++ main.py
@@ -1,48 +1,71 @@
-l_1 = [5, 10, 15, 20, 25, 50, 20]
+# P1
 
-print(l_1)
+l = [5, 10, 15, 20, 25, 50, 20]
+x = l.index(20)
 
-l_2 = [5, 20, 15, 20, 25, 50, 20]
+if 20 in l:
+    l.pop(x)
+    l.insert(x, 200)
+print(l)
 
-l_2.pop(1)
-print(l_2)
+# P2
 
-l_1.insert(3, 200)
-l_1.pop(4)
-print(l_1)
+l_1 = [5, 20, 15, 20, 25, 50, 20]
+y = l_1.index(20)
 
-x1 = [10, 20, 4]
-x2 = [70, 11, 20, 4, 100]
+if 20 in l_1:
+    l_1.pop(y)
+    print(l_1)
 
-x1.sort(reverse=True)
-print(x1[1])
+# P3
+"""
+n = int(input())
+l_2 = []
+for i in range(n):
+    l_2.append(int(input()))
+s = set(l_2)
+s1 = list(l_2)
+s1.sort(reverse=True)
+print(s1[1])
+"""
+# P4
 
-x2.sort(reverse=True)
-print(x2[1])
+dic1 = {1: 10, 2: 20}
+dic2 = {3: 30, 4: 40}
+dic3 = {5: 50, 6: 60}
+dic4 = {}
+dic4.update(dic1)
+dic4.update(dic3)
+dic4.update(dic2)
+print(dic4)
 
-dic1={1:10, 2:20}
-dic2={3:30, 4:40}
-dic3={5:50,6:60}
+# P5
 
-dic1.update(dic2)
-dic1.update(dic3)
-print(dic1)
+dic_remove1 = {'a':1,'b':2,'c':3,'d':4}
+dic_remove1.pop('a')
+print(dic_remove1)
 
-P5 = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+dic_remove2 = {'a':1,'b':2,'c':3,'d':4}
 
-# P5.pop('a')
-# print(P5)
+if 'e' in dic_remove2:
+    dic_remove2.pop('e')
+else:
+    print(dic_remove2)
 
-# P5.pop('e')
-# print(P5)
+# P6
 
-s = {6, 4, 10, 20, 21, 3}
+s = {1, 10, 90, 9}
+n = 90
+if n in s:
+    print(True)
 
-print(s)
+# P7
 
-print(90 in s)
-
-#lst =sdsdjasm,fnd
+n = int(input())
+i = 0
+while i + 7 <= n:
+    i = i + 7
+    print(i)
 
 
 
